48c4714ba0882b84663af16b974206b627d0c46d
kimbogeun <intulda@naver.com>
2020-08-13 11:33:09 +0900

    [add] 학습하기 데이터 통신 메소드 작성 완료

---
+++ src/main/java/io/ticly/mint/learn/LearnController.java
@@ -0,0 +1,58 @@
+package io.ticly.mint.learn;
+
+import io.ticly.mint.learn.model.dto.UserLearnDTO;
+import io.ticly.mint.learn.model.dto.VocaDTO;
+import io.ticly.mint.learn.model.service.LearnService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+import java.util.List;
+
+
+/**
+ * 학습하기 Controller
+ * @create 2020.08.11
+ */
+@Controller
+@RequestMapping(value="/learn/*")
+public class LearnController {
+
+    private LearnService learnService;
+
+    public LearnController(LearnService learnService) {
+        this.learnService = learnService;
+    }
+
+    /**
+     * view page 이동
+     * @param model
+     * @return
+     */
+    @GetMapping(value="workBook")
+    public String view(Model model, int seq) throws SQLException {
+        UserLearnDTO userLearnDTO = UserLearnDTO.builder()
+                .email("test4@naver.com")
+                .articleSeq(seq)
+                .build();
+        learnService.saveUserLearning(userLearnDTO);
+        if(learnService.getUserVocaCheck(userLearnDTO)) {
+            learnService.saveArticleVocaToUser(userLearnDTO);
+        }
+        model.addAttribute("currentArticle", learnService.getArticle(userLearnDTO));
+        return "learn/leaning";
+    }
+
+    /**
+     * UserVocaList를 가져오는 메소드
+     * @param userLearnDTO
+     * @return
+     */
+    @PostMapping(value="getVocaList")
+    @ResponseBody
+    private List<VocaDTO> getVocaList(@RequestBody UserLearnDTO userLearnDTO) {
+        return learnService.getVocaList(userLearnDTO);
+    }
+
+}
