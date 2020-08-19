f57cacded8ee4f534b077d3cde783fcb91f9334b
YUNJIMYEONG <yjim3440@gmail.com>
2020-08-19 14:13:45 +0900

    [edit] oauth 수정 전

--- src/main/webapp/js/member/login.js
+++ src/main/webapp/js/member/login.js
@@ -10,8 +10,8 @@ naver_login_btn.addEventListener("click", function (){
     })
         .then(function (result){
             console.log("데이터 가져오기 성공");
-            window.open(`learn/modal?url=${result.data}`, "네이버 로그인", 'width=500, height=650, status=no, menubar=no, toolbar=no, resizable=no');
-
+            window.open(result.data, "네이버 로그인", 'width=500, height=650, status=no, menubar=no, toolbar=no, resizable=no');
+            /*learn/modal?url=${result.data}*/
         })
         .catch(function (error) {
             console.log("데이터 가져오기 실패");
