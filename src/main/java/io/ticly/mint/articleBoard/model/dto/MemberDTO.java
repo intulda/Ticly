0fa7c1c407bf7f2b7af3bc5b8ab2fde0f925a84d
(Charles) <heycharles@naver.com>
2020-08-20 10:28:19 +0900

    [edit] MemberDTO Setter추가

--- src/main/java/io/ticly/mint/articleBoard/model/dto/MemberDTO.java
+++ src/main/java/io/ticly/mint/articleBoard/model/dto/MemberDTO.java
@@ -1,14 +1,12 @@
 package io.ticly.mint.articleBoard.model.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 
 @Builder
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class MemberDTO {
