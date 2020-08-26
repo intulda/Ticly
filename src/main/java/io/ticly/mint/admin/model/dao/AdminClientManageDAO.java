6c72407f989cb0ec0be1170ba7bc9200b34669cc
YUNJIMYEONG <yjim3440@gmail.com>
2020-08-26 19:21:54 +0900

    [add/clientManage] 회원관리 검색결과 요청 API 작성(전체검색)

--- src/main/java/io/ticly/mint/admin/model/dao/AdminClientManageDAO.java
+++ src/main/java/io/ticly/mint/admin/model/dao/AdminClientManageDAO.java
@@ -2,12 +2,18 @@ package io.ticly.mint.admin.model.dao;
 
 
 import io.ticly.mint.admin.model.dto.AdminClientManageDTO;
+import io.ticly.mint.articleBoard.model.dto.ArticleInfoDTO;
 import io.ticly.mint.learn.model.dto.UserLearnDTO;
 import io.ticly.mint.learn.model.dto.VocaDTO;
 import io.ticly.mint.mypage.model.dto.MyPageDTO;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.List;
 
 @Repository
@@ -25,6 +31,11 @@ public class AdminClientManageDAO {
      */
     public List<AdminClientManageDTO> buttonClientSearch() {
         return  sqlSessionTemplate.selectList("adminClientManageDAO.clientSearchBtn");
+    }
+
+    public List<AdminClientManageDTO> findMemberBySearch(String searchKeyword){
+        System.out.println("DaoSearchKeyword" + searchKeyword);
+        return sqlSessionTemplate.selectList("adminClientManageDAO.findMemberBySearch", searchKeyword);
     }
 }
 
